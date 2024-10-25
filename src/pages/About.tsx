6516da3c3dddefc50afbaf36569317968bd7f8fb
fauzima <fauzimakarimsiregar@gmail.com>
2024-10-25 15:54:01 +0700

    tweak

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,29 @@
+import { Link } from "react-router-dom";
+
 function AboutPage() {
-  return <div></div>;
+  return (
+    <div className="absolute inset-0 place-content-center items-center max-w-screen-lg mx-auto px-5">
+      <div className="flex flex-col justify-between text-center md:h-[60vh] h-[80vh]">
+        <Link
+          to={"/"}
+          className="text-9xl text-gray-200 font-[Helvetica] font-bold tracking-tighter"
+        >
+          LOREM IPSUM
+        </Link>
+        <div className="text-gray-200 font-[Helvetica] font-bold tracking-tighter">
+          This is an About content.
+        </div>
+        <div className="flex justify-evenly items-center text-center">
+          <Link
+            to={"/"}
+            className="text-gray-200 font-[Helvetica] font-bold tracking-tighter"
+          >
+            BACK
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutPage;
